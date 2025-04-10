c87125e898e41ba6ac2b0badd2c049d463744ff0
ghost1382 <127534754+ghost1382@users.noreply.github.com>
2025-04-10 20:48:33 +0200

    Update cita_checker.py

--- cita_checker.py
+++ cita_checker.py
@@ -1,12 +1,9 @@
-from selenium import webdriver
+import undetected_chromedriver.v2 as uc
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
 
 
 def check_appointment():
     print("🟡 Checking for appointment...")
-
-    chromedriver_autoinstaller.install()
 
     options = Options()
     options.add_argument('--headless')
@@ -15,14 +12,10 @@ def check_appointment():
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920x1080')
 
-    driver = webdriver.Chrome(options=options)
+    driver = uc.Chrome(options=options)
 
-    # Open the Cita website
     driver.get("https://sede.administracionespublicas.gob.es/pagina/index/directorio/icpplus")
-
-    # Get the page HTML and close the browser
     html = driver.page_source
     driver.quit()
 
-    # Check if the phrase "No hay citas" appears (meaning no appointments)
-    return "No hay citas" not in html
+    return "No hay citas" not in html  # Just an example
